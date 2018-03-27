0c16bd67fd328689368af4c7ebd03b53583a27ef
matthewdoerksen <matthewdoerksen@users.noreply.github.com>
2018-03-26 21:41:50 -0700

    Update snes_controller_attach.sh

--- snes_controller_attach.sh
+++ snes_controller_attach.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # 0x0079:0x0011 SNES USB controller
-VENDOR_ID="0x0079"
-PRODUCT_ID="0x0011"
-BASE_FILE="snes_controller"
-UNIQUE_SEARCH="Dragon"
-VM="Lakka"
+#VENDOR_ID="0x0079"
+#PRODUCT_ID="0x0011"
+#BASE_FILE="snes_controller"
+#UNIQUE_SEARCH="Dragon"
+#VM="Lakka"
 
-# pull the bus for the devices (same since they're on the same USB hub)
+VENDOR_ID="$1"
+PRODUCT_ID="$2"
+BASE_FILE="$3"
+UNIQUE_SEARCH="$4"
+VM="$5"
+
+# pull the bus for the device
 BUS=$(lsusb | grep ${UNIQUE_SEARCH} | cut -d' ' -f2 | cut -d':' -f1 | cut -d' ' -f1 | cut -d'0' -f3 | cut -d$'\n' -f1)
 BUS=($(echo ${BUS} | sed 's/^0*//'))
 
-# pull the device IDs for the controllers
+# pull the device ID(s) (handles multiple duplicate devices such as game controllers)
 DEVICES=($(lsusb | grep ${UNIQUE_SEARCH} | cut -d' ' -f2,4 | cut -d':' -f1 | cut -d' ' -f2))
 
 # go through the device IDs and strip leading 0s since they'll mess with the attach process
