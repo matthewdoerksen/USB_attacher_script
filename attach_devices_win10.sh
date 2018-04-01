8fa72e55cc68ced3529a8ba98833c8545aeca79a
matthewdoerksen <matthewdoerksen@users.noreply.github.com>
2018-04-01 15:41:47 -0700

    Update attach_devices_win10.sh
    Add a wait between the attach -> detach -> attach process to account for delays in KVM

--- attach_devices_win10.sh
+++ attach_devices_win10.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# grep exit code 0 - line found
-#                1 - line NOT found
-#                2 - error occurred during search
 
 # attach Corsair kbd
 /mnt/user/Common/Lakka_Emulation_Station/attach_device.sh 0x1b1c 0x1b15 kbd Corsair Windows_10
@@ -9,7 +6,9 @@ EXITCODE=$(echo "$?")
 # if = 0, then successfully attached, 1 = already attached (no-op), 2 - other error, detach and attempt re-attach
 while [ $EXITCODE -eq 2 ]
 do
+  sleep 3
   /mnt/user/Common/Lakka_Emulation_Station/detach_device.sh 0x1b1c 0x1b15 kbd Corsair Windows_10
+  sleep 3
   /mnt/user/Common/Lakka_Emulation_Station/attach_device.sh 0x1b1c 0x1b15 kbd Corsair Windows_10
   EXITCODE=$(echo "$?")
   echo "Exit code attempting to detach and re-attach device: $EXITCODE"
@@ -22,7 +21,9 @@ EXITCODE=$(echo "$?")
 # if = 0, then successfully attached, 1 = already attached (no-op), 2 - other error, detach and attempt re-attach
 while [ $EXITCODE -eq 2 ]
 do
+  sleep 3
   /mnt/user/Common/Lakka_Emulation_Station/detach_device.sh 0x0461 0x4d99 mouse Primax Windows_10
+  sleep 3
   /mnt/user/Common/Lakka_Emulation_Station/attach_device.sh 0x0461 0x4d99 mouse Primax Windows_10
   EXITCODE=$(echo "$?")
   echo "Exit code attempting to detach and re-attach device: $EXITCODE"
@@ -35,7 +36,9 @@ EXITCODE=$(echo "$?")
 # if = 0, then successfully attached, 1 = already attached (no-op), 2 - other error, detach and attempt re-attach
 while [ $EXITCODE -eq 2 ]
 do
+  sleep 3
   /mnt/user/Common/Lakka_Emulation_Station/detach_device.sh 0x0c76 0x161f microphone JMTek Windows_10
+  sleep 3
   /mnt/user/Common/Lakka_Emulation_Station/attach_device.sh 0x0c76 0x161f microphone JMTek Windows_10
   EXITCODE=$(echo "$?")
   echo "Exit code attempting to detach and re-attach device: $EXITCODE"
