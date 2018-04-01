1afda77c5ec6a5b672dc36eef09f56c12959202c
matthewdoerksen <matthewdoerksen@users.noreply.github.com>
2018-03-31 18:13:02 -0700

    Update attach_device.sh

--- attach_device.sh
+++ attach_device.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# EXIT CODES
+# 0 - attached device successfully
+# 1 - already attached (no-op)
+# 2 - not attached successfully
+
 # 0x0079:0x0011 SNES USB controller
 #VENDOR_ID="0x0079"
 #PRODUCT_ID="0x0011"
@@ -33,5 +38,24 @@ for ((i=0;i<${#DEVICES[@]};i++)); do
   echo "Wrote XML file: $DEVICE_CONFIG"
 
   echo "Attempting to attach device (Vendor): ${VENDOR_ID}, (Product): ${PRODUCT_ID}, (Bus): ${BUS}, (ID): ${DEVICES[i]}"
-  virsh attach-device "$VM" "$DEVICE_CONFIG"
+  OUTPUT=$(virsh attach-device "$VM" "$DEVICE_CONFIG" 2>&1)
+  EXIT_CODE=$(echo "$?")
+
+  echo "Did the device attach successfully? Exit code: $EXIT_CODE"
+  if [ $EXIT_CODE -gt 0 ]; then
+
+    # turn our unsuccessful exit code + text into one of our exit codes
+    # is it already attached?
+    echo "$OUTPUT" | grep "is in use by driver QEMU"
+    EXIT_CODE=$(echo "$?")
+
+    if [ $EXIT_CODE -eq 0 ]; then
+      echo "Device already attached, exiting with exit code 1"
+      exit 1
+    fi
+
+    # exit with some other code
+    exit 2
+  fi
+  # if we get here the device attached successfully, either exit or continue with the next device
 done
